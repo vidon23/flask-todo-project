13dac0770d0be5f5a5191396e563d4a3c392195b
vidon23 <vidon083@gmail.com>
2026-07-05 20:56:30 +0530

    Added submittodoitem route

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ def api():
     }
     return jsonify(data)
 
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+    itemName = "demo item"
+    itemDescription = "demo description"
+
+    return jsonify({
+        "itemName": itemName,
+        "itemDescription": itemDescription,
+        "message": "Todo saved"
+    })
 if __name__ == "__main__":
     app.run(debug=True)
